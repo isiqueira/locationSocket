af97e595509cd1e7ba95eb63b7b0439c9afec2cd
isiqueira <siqueira.italo@gmail.com>
2019-05-21 16:57:39 -0400

    chanto bd to mlab

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 
 
 module.exports = {
-    connString: 'mongodb://localhost:17017/conat',
+    connString: 'mongodb://jcug:jc1x1x1x@ds062097.mlab.com:62097/conat',
 };
